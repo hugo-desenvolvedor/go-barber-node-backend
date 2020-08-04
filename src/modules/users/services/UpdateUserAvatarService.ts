25e67a6ae81ef9f4fa234db8ae11ef9f699afbc7
Hugo Santos <hugo.desenvolvedor@gmail.com>
2020-08-04 07:52:08 -0300

    Finalizing the unit Tests

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -4,11 +4,12 @@ import fs from 'fs';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import { injectable, inject } from 'tsyringe';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 
 interface IRequest {
-    user_id: string;
-    avatar_file_name: string;
+    userId: string;
+    avatarFileName: string;
 }
 
 @injectable()
@@ -16,13 +17,13 @@ class UpdateUserAvatarService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
+
+        @inject('StorageProvider')
+        private storageProvider: IStorageProvider,
     ) {}
 
-    public async execute({
-        user_id,
-        avatar_file_name,
-    }: IRequest): Promise<User> {
-        const user = await this.usersRepository.findById(user_id);
+    public async execute({ userId, avatarFileName }: IRequest): Promise<User> {
+        const user = await this.usersRepository.findById(userId);
 
         if (!user) {
             throw new AppError(
@@ -32,20 +33,12 @@ class UpdateUserAvatarService {
         }
 
         if (user.avatar) {
-            const userAvatarFilePath = path.join(
-                uploadConfig.directory,
-                user.avatar,
-            );
-            const userAvatarFileExists = await fs.promises.stat(
-                userAvatarFilePath,
-            );
-
-            if (userAvatarFileExists) {
-                await fs.promises.unlink(userAvatarFilePath);
-            }
+            await this.storageProvider.deleteFile(user.avatar);
         }
 
-        user.avatar = avatar_file_name;
+        const filename = await this.storageProvider.saveFile(avatarFileName);
+
+        user.avatar = filename;
 
         await this.usersRepository.save(user);
 
